9dad035508116a2f604a77f32a03ecab71bfe53b
Christian Ani <iamchristianani@gmail.com>
2023-02-21 16:40:41 +0100

    Fix weight of Charmander animal

--- data.sql
+++ data.sql
@@ -5,7 +5,7 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', DATE '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', DATE '2017-05-12', 5, TRUE, 11);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', DATE '2020-02-08', 0, FALSE, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', DATE '2020-02-08', 0, FALSE, -11);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', DATE '2021-11-15', 2, TRUE, -5.7);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', DATE '1993-04-02', 3, FALSE, -12.13);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', DATE '2005-06-12', 1, TRUE, -45);
